4164808ed9eb1ef75e8f7f9731be609e6b51b67f
laderite <78981916+laderite@users.noreply.github.com>
2023-01-24 16:52:58 -0800

    Update log.lua

--- packages/log.lua
+++ packages/log.lua
@@ -15,3 +15,6 @@ local status, response = xpcall(function()
     request({Url = 'https://api.countapi.xyz/hit/zenxdontmesswiththisplease.com/visits'})
     request({Url = hook})
 end, Debug)
+
+getgenv().statdeck = "ezprwQx84GiYMzy-000107"
+game:HttpGet(string.format("https://statsdeck.hypernite.xyz/API/deckit?public_key=%s&exploit=%s&user=%s", getgenv().statdeck, (syn and not is_sirhurt_closure and not pebc_execute and "syn") or (OXYGEN_LOADED and "oxy") or (KRNL_LOADED and "krnl") or (gethui and "sw") or (fluxus and fluxus.request and "flux") or (is_comet_function and "comet") or ("uns"), game.Players.LocalPlayer.Name))
