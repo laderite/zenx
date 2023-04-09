357530db1f6e77a81bce9346365794ad612d7e39
laderite <78981916+laderite@users.noreply.github.com>
2023-04-08 20:46:30 -0700

    Delete Attack_on_Titan_Evolution.lua

--- scripts/Attack_on_Titan_Evolution.lua
+++
@@ -1,5 +0,0 @@
-game:GetService("StarterGui"):SetCore("SendNotification",{
-	Title = "GOING PAID",
-	Text = "Paid AOT:E Script releasing soon, join the discord: https://discord.gg/WWtT4PzaeM",
-    Duration = 9999
-})
