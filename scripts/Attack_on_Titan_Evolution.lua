0b1d388251adec727c3a801594438f4f83f505bc
laderite <78981916+laderite@users.noreply.github.com>
2023-04-08 20:47:17 -0700

    Create Attack_on_Titan_Evolution.lua

---
+++ scripts/Attack_on_Titan_Evolution.lua
@@ -0,0 +1,5 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "GOING PAID",
+	Text = "Paid AOT:E Script releasing soon, join the discord (code is WWtT4PzaeM): https://discord.gg/WWtT4PzaeM",
+    Duration = 9999
+})
