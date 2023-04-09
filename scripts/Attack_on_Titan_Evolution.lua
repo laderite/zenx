0b4b4cbe355ab4d017d4b492b06677e3fa84e478
laderite <78981916+laderite@users.noreply.github.com>
2023-04-09 09:12:57 -0700

    Update Attack_on_Titan_Evolution.lua

--- scripts/Attack_on_Titan_Evolution.lua
+++ scripts/Attack_on_Titan_Evolution.lua
@@ -1,3 +1,4 @@
+
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "NEW AND IMPROVED PAID SCRIPT",
 	Text = "Join the discord (copied to clipboard) to purchase a NEW and IMPROVED AOT:E script with auto missions, auto raids and auto waves. + You get rewards!",
